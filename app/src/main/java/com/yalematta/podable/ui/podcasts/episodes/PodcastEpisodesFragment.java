2230b3814c085028f2ff7b9412a82f1b10836eac
yalematta <layale.matta@gmail.com>
2018-07-22 00:55:02 +0300

    Fixed Podcast Episodes RecyclerView design

--- app/src/main/java/com/yalematta/podable/ui/podcasts/episodes/PodcastEpisodesFragment.java
+++ app/src/main/java/com/yalematta/podable/ui/podcasts/episodes/PodcastEpisodesFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -53,6 +54,7 @@ public class PodcastEpisodesFragment extends Fragment implements PodcastEpisodes
         mPresenter.getEpisodes(currentPodcast.getId(), 0, 10);
         linearLayoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this.getActivity(), LinearLayout.VERTICAL));
     }
 
     @Override
